011fb968ed61dc623a21aafa126626e1fb958d90
rb6422 <robertotheboss6422@gmail.com>
2023-11-09 21:37:15 -0400

    fix: Correct RentMovie function
    Corrected rentmovie to search for the movie to rent in movies.csv using binary search and make the rental

--- src/lib/filter.cpp
+++ src/lib/filter.cpp
@@ -239,28 +239,66 @@ void RentMovie(Movie movies[], int n) {
     cout << "Enter the ID of the movie to rent: ";
     cin >> id;
 
-    if (id >= 1 && id <= n) {
-        if (movies[id - 1].status == "Available") {
-            cout << "Enter your name: ";
-            cin.ignore();
-            getline(cin, clientName);
+    // Open the movies.csv file for reading
+    ifstream inFile("movies.csv");
+    if (!inFile) {
+        cout << "Failed to open movies.csv" << endl;
+        return;
+    }
 
-            movies[id - 1].rentTo = clientName;
-            movies[id - 1].status = "Rented";
-            
-            ofstream outFile("movies.csv");
-            for (int i = 0; i < n; i++) {
-                outFile << movies[i].id << "," << movies[i].name << "," << movies[i].Genre << ","
+    int start = 0, end = n - 1;
+    
+    bool movieFound = false;
+
+    while(start <= end){
+        int mid = start + (end - start) / 2;
+
+            string line;
+            getline(inFile, line);
+            stringstream ss(line);
+            string token;
+            getline(ss, token, ';');
+            int movieId = stoi(token);
+
+            if (movieId == id) {
+                movieFound = true;
+                getline(ss, token, ';');
+                string name = token;
+                cout << "Movie found: " << name << endl;
+                cout << "ID: " << movieId << endl;
+        
+                cout << "Enter your name: ";
+                cin.ignore();
+                getline(cin, clientName);
+
+                movies[mid].rentTo = clientName;
+                movies[mid].status = "Rented";
+
+                // Update the movies.csv file with the rented movie
+                ofstream outFile("movies.csv");
+                if (!outFile) {
+                    cout << "Failed to open movies.csv for writing" << endl;
+                    return ;
+                }
+                for (int i = 0; i < n; i++) {
+                    outFile << movies[i].id << "," << movies[i].name << "," << movies[i].Genre << ","
                         << movies[i].duration << "," << movies[i].director << "," << movies[i].releaseDate << ","
                         << movies[i].rentTo << "," << movies[i].rentOn << "," << movies[i].status << endl;
-            }
-            outFile.close();
+                }
+                outFile.close();
 
-            cout << "Movie rented successfully to " << clientName << endl;
-        } else {
-            cout << "Movie is not available for rent." << endl;
+                cout << "Movie rented successfully to " << clientName << endl;
+                break;
+            }else if (movieId < id){
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }    
         }
-    } else {
-        cout << "Invalid movie ID." << endl;
+
+    inFile.close();
+
+    if (!movieFound) {
+        cout << "Movie not found." << endl;
     }
 }
